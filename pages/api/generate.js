84b3e7a0b2a2becc6289da97af9592004d64bca7
Kirill Lazarev <klazarev2000@gmail.com>
2023-05-24 19:10:10 -0700

    added textarea

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,18 +9,30 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const resume = req.body.resume || "";
+  const jobDescription = req.body.jobDescription || "";
+
+  if (resume.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
-      }
+        message: "Please provide a valid resume",
+      },
+    });
+    return;
+  }
+
+  if (jobDescription.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please provide a valid job description",
+      },
     });
     return;
   }
@@ -32,7 +44,7 @@ export default async function (req, res) {
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -41,22 +53,13 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(resume, jobDescription) {
+  return `Tailor my resume: ${resume} to my job description: ${jobDescription}`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [resume, setResume] = useState("");
+  const [jobDescription, setJobDescription] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,17 +15,23 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({
+          resume: resume,
+          jobDescription: jobDescription,
+        }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
       setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
+      setResume("");
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
@@ -34,22 +41,41 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>ResumeGPT</title>
+        <script
+          src="https://kit.fontawesome.com/e5790a7f1d.js"
+          crossorigin="anonymous"
+        ></script>
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <i className="fa-light fa-file"></i>
+
+        <h3>Tailor My Resume</h3>
         <form onSubmit={onSubmit}>
+          <label for="resume_upload">
+            Upload your resume (.doc, .docx, .pdf)
+          </label>
           <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            type="file"
+            id="resume_upload"
+            accept=".doc,.docx,.pdf"
+            name="resume"
+            required
+            value={resume}
+            onChange={(e) => setResume(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          {/* TODO: create the textarea wider */}
+          <textarea
+            name="jobDescription"
+            id="description_upload"
+            cols="40"
+            rows="10"
+            required
+            value={jobDescription}
+            onChange={(e) => setJobDescription(e.target.value)}
+          ></textarea>
+          <input type="submit" value="Generate" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
