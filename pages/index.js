9ca5ccc299173dd04a0a686108ac96eda71ebaec
Kirill Lazarev <klazarev2000@gmail.com>
2023-07-02 19:01:28 -0700

    basic download feature done

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,53 @@
 import Head from "next/head";
 import styles from "./index.module.css";
 import { useState } from "react";
-import Link from 'next/link'
-import { useSession, getSession, signOut } from "next-auth/react"
-
+import Link from "next/link";
+import { useSession, getSession, signOut } from "next-auth/react";
+import { saveAs } from "file-saver";
 
 export default function Home() {
   const [result, setResult] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const {data: session} = useSession()
+  const { data: session } = useSession();
 
-  function handleSignOut(){
-    signOut()
+  const downloadFile = () => {
+    const fileContent = result;
+
+    const file = new Blob([fileContent], {
+      type: "text/plain;charset=utf-8",
+    });
+    saveAs(file, "example.txt");
+  };
+
+  function handleSignOut() {
+    signOut();
   }
 
-  function User({session, handleSignOut}){
+  function User({ session, handleSignOut }) {
     return (
       // Need to connect Sign out button to main page below
       <main>
-        <div className='details'>
+        <div className="details">
           <h5>{session.user.name}</h5>
           <h5>{session.user.email}</h5>
         </div>
 
         <div className="flex justify-center">
-          <button onClick={handleSignOut} className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 bg-gray-50">
+          <button
+            onClick={handleSignOut}
+            className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 bg-gray-50"
+          >
             Sign Out
           </button>
         </div>
       </main>
-    )
+    );
   }
 
-  {session ? User({session, handleSignOut}) : null}
+  {
+    session ? User({ session, handleSignOut }) : null;
+  }
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -68,16 +82,18 @@ export default function Home() {
   }
 
   return (
-    
     <div>
-      <div className='details'>
-          <h5>{session.user.name}</h5>
-          <h5>{session.user.email}</h5>
+      <div className="details">
+        <h5>{session.user.name}</h5>
+        <h5>{session.user.email}</h5>
       </div>
       <div className="flex justify-center">
-          <button onClick={handleSignOut} className="absolute left-0 px-10 py-1 rounded-sm bg-green-300">
-            Sign Out
-          </button>
+        <button
+          onClick={handleSignOut}
+          className="absolute left-0 px-10 py-1 rounded-sm bg-green-300"
+        >
+          Sign Out
+        </button>
       </div>
 
       <Head>
@@ -106,25 +122,27 @@ export default function Home() {
           <input type="submit" value="Generate" />
         </form>
         {loading && <p>Loading ...</p>}
-        <p>{result}</p>
+        {!loading && result && (
+          <button onClick={downloadFile}>Download File</button>
+        )}
       </main>
     </div>
   );
 }
 
-export async function getServerSideProps({req}){
-  const session = await getSession({ req })
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
 
-  if(!session) {
+  if (!session) {
     return {
       redirect: {
-        destination: '/login',
-        permanenet: false
-      }
-    }
+        destination: "/login",
+        permanenet: false,
+      },
+    };
   }
 
   return {
-    props: { session }
-  }
+    props: { session },
+  };
 }
--- pages/login.js
+++ pages/login.js
@@ -1,112 +1,132 @@
-import Head from 'next/head'
-import Layout from '../layout/layout'
-import Link from 'next/link'
-import styles from '../styles/Form.module.css'
-import Image from 'next/image'
+import Head from "next/head";
+import Layout from "../layout/layout";
+import Link from "next/link";
+import styles from "../styles/Form.module.css";
+import Image from "next/image";
 import { HiAtSymbol, HiFingerPrint } from "react-icons/hi";
-import { useState } from 'react'
-import { useSession, signIn, signOut } from "next-auth/react"
-import { useFormik } from 'formik'
-import login_validate from '../lib/validate'
-import { useRouter } from 'next/router'
+import { useState } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useFormik } from "formik";
+import login_validate from "../lib/validate";
+import { useRouter } from "next/router";
 
-export default function Login(){
+export default function Login() {
+  const [show, setShow] = useState(false);
+  const router = useRouter();
+  // formik hook
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: login_validate,
+    onSubmit,
+  });
 
-    const [show,setShow] = useState(false)
-    const router = useRouter()
-    // formik hook
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: ''
-        },
-        validate : login_validate,
-        onSubmit
-    })
+  async function onSubmit(values) {
+    const status = await signIn("credentials", {
+      redirect: false,
+      email: values.email,
+      password: values.password,
+      callbackUrl: "/",
+    });
 
-    console.log(formik.errors)
+    if (status.ok) router.push(status.url);
+  }
 
-    async function onSubmit (values) {
-        const status = await signIn('credentials', {
-            redirect: false,
-            email: values.email,
-            password: values.password,
-            callbackUrl: "/"
-        })
+  // Google handler function
 
-        if(status.ok) router.push(status.url)
-    }
+  async function handleGoogleSignin() {
+    signIn("google", { callbackUrl: "https://resumegpt-rho.vercel.app" });
+  }
 
-    // Google handler function
+  return (
+    <Layout>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <section className="w-3/4 mx-auto flex flex-col gap-10">
+        <div className="title">
+          <h1 className="text-gray-800 text-4xl font-bold py-4">ResumeGPT</h1>
+          <p className="w-3/4 mx-auto text-gray-400">
+            Login to access AI Resume Tailoring
+          </p>
+        </div>
 
-    async function handleGoogleSignin(){
-        signIn('google', {callbackUrl: "https://resumegpt-rho.vercel.app"})
-    }
+        <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
+          {/* Input fields */}
+          <div className={styles.input_group}>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className={styles.input_text}
+              {...formik.getFieldProps("email")}
+            />
+            <span className="icon flex items-center px-4">
+              <HiAtSymbol size={25} />
+            </span>
+          </div>
+          {formik.errors.email && formik.touched.email ? (
+            <span className="text-rose-500">{formik.errors.email}</span>
+          ) : (
+            <></>
+          )}
+          <div className={styles.input_group}>
+            <input
+              type={`${show ? "text" : "Password"}`}
+              name="password"
+              placeholder="Password"
+              className={styles.input_text}
+              {...formik.getFieldProps("password")}
+            />
+            <span
+              className="icon flex items-center px-4"
+              onClick={() => setShow(!show)}
+            >
+              <HiFingerPrint size={25} />
+            </span>
+          </div>
+          {formik.errors.password && formik.touched.password ? (
+            <span className="text-rose-500">{formik.errors.password}</span>
+          ) : (
+            <></>
+          )}
 
-    return (
-
-        <Layout>
-
-            <Head>
-                <title>Login</title>
-            </Head>
-        <section className='w-3/4 mx-auto flex flex-col gap-10'>
-            <div className="title">
-                <h1 className='text-gray-800 text-4xl font-bold py-4'>ResumeGPT</h1>
-                <p className='w-3/4 mx-auto text-gray-400'>Login to access AI Resume Tailoring</p>
-            </div>
-
-            <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
-                {/* Input fields */}
-                <div className={styles.input_group}>
-                    <input
-                    type="email"
-                    name="email"
-                    placeholder="Email" 
-                    className = {styles.input_text}
-                    {...formik.getFieldProps('email')}
-                    />
-                    <span className='icon flex items-center px-4'>
-                      <HiAtSymbol size={25}/>  
-                    </span>
-                </div>
-                {formik.errors.email && formik.touched.email ? <span className='text-rose-500'>{formik.errors.email}</span> : <></>}
-                <div className={styles.input_group}>
-                    <input
-                    type={`${show ? "text": "Password"}`}
-                    name="password"
-                    placeholder="Password"
-                    className = {styles.input_text}
-                    {...formik.getFieldProps('password')}
-                    />
-                    <span className='icon flex items-center px-4' onClick={() => setShow(!show)}>
-                      <HiFingerPrint size={25}/>  
-                    </span>
-                </div>
-                {formik.errors.password && formik.touched.password ? <span className='text-rose-500'>{formik.errors.password}</span> : <></>}
-
-                {/* Login Buttons */}
-                <div className="input-button">
-                    <button type="submit" className={styles.button}>
-                        Login
-                    </button>
-                </div>
-                <div className="input-button">
-                    <button type="button" onClick={handleGoogleSignin} className={styles.button_custom}>
-                        Sign in With Google <Image src={'/assets/google-logo.svg'} width="20" height={20}></Image>
-                    </button>
-                </div>
-                {/* <div className="input-button">
+          {/* Login Buttons */}
+          <div className="input-button">
+            <button type="submit" className={styles.button}>
+              Login
+            </button>
+          </div>
+          <div className="input-button">
+            <button
+              type="button"
+              onClick={handleGoogleSignin}
+              className={styles.button_custom}
+            >
+              Sign in With Google{" "}
+              <Image
+                src={"/assets/google-logo.svg"}
+                width="20"
+                height={20}
+              ></Image>
+            </button>
+          </div>
+          {/* <div className="input-button">
                     <button type="button" className={styles.button_custom}>
                         Sign in with GitHub <Image src={'/assets/github-mark.svg'} width="20" height={20}></Image>
                     </button>
                 </div> */}
-            </form>
+        </form>
         {/* bottom */}
         <p className="text-center text-gray-400">
-            Don't have an account yet? <Link legacyBehavior href={'/register'}><a className='text-green-500'>Sign Up</a></Link>
+          Don't have an account yet?{" "}
+          <Link legacyBehavior href={"/register"}>
+            <a className="text-green-500">Sign Up</a>
+          </Link>
         </p>
-        </section>
-        </Layout>
-    )
+      </section>
+    </Layout>
+  );
 }
