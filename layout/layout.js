47a4028ff31cadfdd1d37bfe42364d309031526e
Pavan <pavan.khiani@gmail.com>
2023-07-17 13:43:40 -0400

    home page and navbar cleanup

--- layout/layout.js
+++ layout/layout.js
@@ -2,7 +2,7 @@ import styles from '../styles/Layout.module.css'
 
 export default function Layout({children}){
     return (
-        <div className="flex h-screen bg-green-300 ">
+        <div className="flex h-screen bg-white ">
             <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4">
                 {/* <div className={styles.imgStyle}>
                     <div className={styles.cartoonImg}></div>
---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import Hero from "../components/Hero";
+
+export default function About() {
+    return (
+        <div></div>
+    )
+}
