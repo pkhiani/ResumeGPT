042461f3ecdf8d90941e015f83434959df5ff1f4
Pavan <pavan.khiani@gmail.com>
2023-07-20 13:51:35 -0400

    contact page setup

---
+++ pages/contact.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import ContactUs from "../components/Contact";
+
+export default function Contact() {
+    return (
+
+        <div>
+            <Head>
+            <title>Contact</title>
+            </Head>
+        
+            <ContactUs></ContactUs>
+        </div>
+
+    )
+}
